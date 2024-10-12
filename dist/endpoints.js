31f6f253378152ee8d34f4b1e33e5f7e21615029
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2024-10-12 16:26:57 +0800

    build .

--- dist/endpoints.js
+++ dist/endpoints.js
@@ -1,3 +1,4 @@
 "use strict";
-export const listAssociatedPullRequests = "GET /repos/{owner}/{repo}/commits/{commit_sha}/pulls";
+export const getPullRequest = "GET /repos/{owner}/{repo}/pulls/{pull_number}";
 export const updatePullRequest = "PATCH /repos/{owner}/{repo}/pulls/{pull_number}";
+export const listWorkflowRunArtifacts = "GET /repos/{owner}/{repo}/actions/runs/{run_id}/artifacts";
--- dist/index.js
+++ dist/index.js
@@ -1,31 +1,41 @@
 "use strict";
 import core from "@actions/core";
 import github from "@actions/github";
-import { listAssociatedPullRequests, updatePullRequest } from "./endpoints.js";
-import { exit } from "process";
+import { getPullRequest, listWorkflowRunArtifacts, updatePullRequest } from "./endpoints.js";
 const inputs = {
   githubToken: core.getInput("github_token", { required: true }),
-  repoOwner: core.getInput("repo_owner"),
-  repoName: core.getInput("repo_name")
+  repoOwner: core.getInput("repo_owner") || github.context.repo.owner,
+  repoName: core.getInput("repo_name") || github.context.repo.repo,
+  runId: github.context.runId
 };
 const octokit = github.getOctokit(inputs.githubToken);
-const owner = inputs.repoOwner === "" ? github.context.repo.owner : inputs.repoOwner;
-const repo = inputs.repoName === "" ? github.context.repo.repo : inputs.repoName;
-const runId = github.context.runId;
-const associatedPrs = await octokit.request(listAssociatedPullRequests, {
-  owner,
-  repo,
-  commit_sha: github.context.sha
+const parsePullRequestNumber = (githubRef) => {
+  const result = /refs\/pull\/(\d+)\/merge/g.exec(githubRef);
+  if (!result) throw new Error("Reference not found.");
+  const [, pullRequestId] = result;
+  return parseInt(pullRequestId);
+};
+const prNumber = parsePullRequestNumber(github.context.ref);
+const getPrResp = await octokit.request(getPullRequest, {
+  owner: inputs.repoOwner,
+  repo: inputs.repoName,
+  pull_number: prNumber
 });
-if (associatedPrs.data.length === 0) {
-  core.error("No PRs associated with this commit.");
-  core.setFailed("No PRs associated with this commit.");
-  exit(0);
+const pr = getPrResp.data;
+const artifactsResp = await octokit.request(listWorkflowRunArtifacts, {
+  owner: inputs.repoOwner,
+  repo: inputs.repoName,
+  run_id: inputs.runId
+});
+const artifacts = artifactsResp.data.artifacts;
+var links = "";
+for (const artifact of artifacts) {
+  links += artifact.archive_download_url + "\n";
 }
-const pr = associatedPrs.data[0];
 await octokit.request(updatePullRequest, {
-  owner,
-  repo,
+  owner: inputs.repoOwner,
+  repo: inputs.repoName,
   pull_number: pr.number,
-  body: pr.body + " updated body"
+  body: pr.body + `
+${links}`
 });
